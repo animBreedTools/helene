16372da42b9c78a03856aa5b9dc30bfc04e673d5
Emre Karaman <emrkaraman@gmail.com>
2019-09-23 10:58:43 +0200

    Update helene.jl

--- helene.jl
+++ helene.jl
@@ -1,1 +1,70 @@
-xsdcsd
+using DataFrames
+using Distributions
+using Printf
+
+function bayesPR(genoTrain, phenoTrain, snpInfo, chrs, fixedRegSize, varGenotypic, varResidual, chainLength, burnIn, outputFreq, onScreen)
+    SNPgroups, genoX = prepRegionData(snpInfo, chrs, genoTrain, fixedRegSize)
+    these2Keep = collect((burnIn+outputFreq):outputFreq:chainLength) #print these iterations
+    nRegions    = length(SNPgroups)
+    println("number of regions: ", nRegions)
+    dfEffectVar = 4.0
+    dfRes       = 4.0
+    X           = convert(Array{Float64}, genoX[2:end])
+    println("X is this size", size(X))
+    y           = convert(Array{Float64}, phenoTrain)
+    println("y is this size", size(y))
+    nTraits, nRecords , nMarkers   = size(y,2), size(y,1), size(X,2)
+    fileControlSt(fixedRegSize)
+    p           = mean(X,dims=1)./2.0
+    sum2pq      = sum(2*(1 .- p).*p)
+    if varGenotypic==0.0
+        varBeta      = fill(0.0005, nRegions)
+        else varBeta = fill(varGenotypic/sum2pq, nRegions)
+    end
+    if varResidual==0.0
+        varResidual  = 0.0005
+    end
+    scaleVar        = varBeta[1]*(dfEffectVar-2.0)/dfEffectVar
+    νS_β            = scaleVar*dfEffectVar
+    df_β            = dfEffectVar
+    scaleRes        = varResidual*(dfRes-2.0)/dfRes
+    νS_e            = scaleRes*dfRes
+    df_e            = dfRes
+    tempBetaVec     = zeros(Float64,nMarkers) #initial values as "0"
+    μ               = mean(y)
+    X              .-= ones(Float64,nRecords)*2p
+    xpx             = diag(X'X)
+    ycorr           = y .- μ
+    #MCMC starts here
+    for iter in 1:chainLength
+        #sample residual variance
+        varE = sampleVarE(νS_e,ycorr,df_e,nRecords)
+        #sample intercept
+        ycorr    .+= μ
+        rhs      = sum(ycorr)
+        invLhs   = 1.0/nRecords
+        meanMu   = rhs*invLhs
+        μ        = rand(Normal(meanMu,sqrt(invLhs*varE)))
+        ycorr    .-= μ
+        for r in 1:nRegions
+            theseLoci = SNPgroups[r]
+            regionSize = length(theseLoci)
+            λ_r = varE/varBeta[r]
+            for l in theseLoci::UnitRange{Int64}
+                BLAS.axpy!(tempBetaVec[l], view(X,:,l), ycorr)
+                rhs = view(X,:,l)'*ycorr
+                lhs = xpx[l] + λ_r
+                meanBeta = lhs\rhs
+                tempBetaVec[l] = sampleBeta(meanBeta, lhs, varE)
+                BLAS.axpy!(-1*tempBetaVec[l], view(X,:,l), ycorr)
+            end
+            varBeta[r] = sampleVarBeta(νS_β,tempBetaVec[theseLoci],df_β,regionSize)
+        end
+        outputControlSt(onScreen,iter,these2Keep,X,tempBetaVec,μ,varBeta,varE,fixedRegSize)
+    end
+#    betaFromFile =  readcsv(pwd()"/betaOut",header=false)
+#    print("read $(size(betaFromFile,1)) samples for $(size(betaFromFile,2)) markers from betaOut \n")
+#    bayesRegOut = mean(betaFromFile,1)
+#    @printf("acc %.6f \n", cor(y,X*bayesRegOut')[1])
+end
+
